ce4c8e91c2b44d93b915be980da7108337b63e71
xfree123 <sanbangtatca022015@gmail.com>
2018-04-04 18:45:45 +0700

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 echo "=========================This will extract all file=================================="
-cd /usr/local/src/nginx/
+cd /tmp/nginx
 unzip -oq echo-nginx-module.zip
 rm -rf echo-nginx-module.zip
 unzip -oq ngx_http_substitutions_filter_module.zip
@@ -13,8 +12,8 @@ rm -f headers-more-nginx-module.zip
 tar -xzvf openssl-1.1.1-pre4.tar.gz
 tar -xzvf openssl-1.0.2l.tar.gz
 rm -rf openssl*.tar.gz
-tar -xzvf zlib*.tar.gz
-rm -rf zlib*.tar.gz
+tar -xzvf zlib-1.2.11.tar.gz
+rm -rf zlib-1.2.11.tar.gz
 unzip -oq pcre-8.41.zip
 rm -rf pcre-8.41.zip
 tar -xzvf ngx_http_redis-0.3.8.tar.gz
@@ -44,4 +43,3 @@ unzip -oq initscript-nginx.zip
 rm -f initscript-nginx.zip
 unzip -oq nginx-limit-upstream-master.zip
 rm -f nginx-limit-upstream-master.zip
-
