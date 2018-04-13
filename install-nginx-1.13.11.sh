098bbeba3143b8d14f608d59c093a1d15eb55cb0
xfree123 <sanbangtatca022015@gmail.com>
2018-04-13 15:52:33 +0700

    Update install-nginx-1.13.11.sh

--- install-nginx-1.13.11.sh
+++ install-nginx-1.13.11.sh
@@ -11,8 +11,8 @@ patch -p1 < ../nginx-limit-upstream-master/nginx-1.12.1.patch
 --group=nginx --user=nginx \
 --pid-path=/var/run/nginx.pid \
 --conf-path=/etc/nginx/nginx.conf \
---http-log-path=/var/log/nginx \
---error-log-path=/var/log/nginx \
+--http-log-path=/var/log/nginx/access.log \
+--error-log-path=/var/log/nginx/error.log \
 --with-http_v2_module \
 --with-http_ssl_module \
 --with-http_stub_status_module \
